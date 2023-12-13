27d6b0184cfaa02a60a1e5277f8f5f304c074da2
agent19music <seanmotanya@gmail.com>
2023-12-13 11:10:37 +0300

    Done with assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Mr. Chocolate", 12, "M","Brown","Phlegmatic",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 17, "M", "White", "Sanguine", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 21, "M", "White", "Melancholic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, "M", "Black", "Phlegmatic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "White", "Sanguine", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 16, "F", "Black", "Choleric", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 11, "F", "Black", "Phlegmatic", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, "F", "Black", "Phlegmatic", FALSE);
+
+
+
